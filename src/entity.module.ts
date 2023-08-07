6fade3fb35e581def6ab220487180412ea904773
d-o0o-b11 <jimin8830@naver.com>
2023-08-08 00:11:32 +0900

    ✨ - 폴더명 변경

--- src/entity.module.ts
+++ src/entity.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserUrlEntity } from './user-url/entities/user-url.entity';
 import { UserInfoEntity } from './user-url/entities/user-info.entity';
-import { AdjectiveExpressionEntity } from './game-kind/entities/game-kind.entity';
+import { AdjectiveExpressionEntity } from './game-kind/entities/adjective-expression.entity';
 
 @Module({
   imports: [
--- src/game-kind/game-kind.service.ts
+++ src/game-kind/game-kind.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AdjectiveExpressionEntity } from './entities/game-kind.entity';
+import { AdjectiveExpressionEntity } from './entities/adjective-expression.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
