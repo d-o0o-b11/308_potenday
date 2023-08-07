9d4c8d173647f524adf22c5d2ef9fc4d707d2c80
d-o0o-b11 <jimin8830@naver.com>
2023-08-07 20:58:25 +0900

    📦 - config, swagger install

--- src/user-url/user-url.controller.ts
+++ src/user-url/user-url.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserUrlService } from './user-url.service';
 import { CreateUserUrlDto } from './dto/create-user-url.dto';
 import { UpdateUserUrlDto } from './dto/update-user-url.dto';
@@ -6,29 +14,4 @@ import { UpdateUserUrlDto } from './dto/update-user-url.dto';
 @Controller('user-url')
 export class UserUrlController {
   constructor(private readonly userUrlService: UserUrlService) {}
-
-  @Post()
-  create(@Body() createUserUrlDto: CreateUserUrlDto) {
-    return this.userUrlService.create(createUserUrlDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userUrlService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userUrlService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserUrlDto: UpdateUserUrlDto) {
-    return this.userUrlService.update(+id, updateUserUrlDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userUrlService.remove(+id);
-  }
 }
