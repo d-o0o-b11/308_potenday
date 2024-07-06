4212eb8a87ef26a3b564f6e587388181d79224dd
d-o0o-b11 <jimin8830@naver.com>
2024-07-06 15:20:26 +0900

    ✨ - global filter queryRunner, internet error 추가

--- server/src/common/filter/error.filter.ts
+++ server/src/common/filter/error.filter.ts
@@ -1,17 +1,44 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { BasicException } from '../error';
-import { Response } from 'express';
+import { BasicException } from '@common/error';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { QueryFailedError } from 'typeorm';
 
-@Catch(BasicException)
-export class BasicExceptionFilter implements ExceptionFilter {
-  catch(exception: BasicException, host: ArgumentsHost): any {
+@Catch(HttpException, QueryFailedError)
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = (exception as any).message;
+    let code: string;
 
-    response.status(exception.getStatus()).json({
-      code: exception.code,
-      status: exception.getStatus(),
-      message: exception.message,
+    if (exception instanceof BasicException) {
+      status = exception.getStatus();
+      code = exception.code;
+      message = exception.message;
+    } else if (exception instanceof HttpException) {
+      code = 'INTERNET_SERVER_ERROR';
+      status = exception.getStatus();
+      message = exception.getResponse();
+    } else if (exception instanceof QueryFailedError) {
+      code = 'QUERY_ERROR';
+      status = HttpStatus.BAD_REQUEST;
+      message = 'Database query failed';
+    }
+
+    response.status(status).json({
+      code: code,
+      status: status,
+      timestamp: new Date().toISOString(),
+      path: `${request.method} ${request.url}`,
+      message: message,
     });
   }
 }
