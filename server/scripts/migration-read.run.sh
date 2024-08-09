0b6e3f6c8cdc59f49955e31970fdaf7cdee7d7ad
d-o0o-b11 <jimin8830@naver.com>
2024-08-09 23:17:57 +0900

    ✅ - read DB migrattion

---
+++ server/scripts/migration-read.run.sh
@@ -0,0 +1,6 @@
+export NODE_ENV=$1
+
+yarn env-cmd -f ./envs/$1.env \
+    ts-node -r tsconfig-paths/register ./node_modules/typeorm/cli.js \
+    -d=./src/database/config/read-typeorm.config.ts \
+    migration:run
